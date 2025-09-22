b6888e3783e1bd593d890604771f3bae0ebca872
Ferenc Deak <deak_ferenc_jozsef@yahoo.com>
2025-09-22 17:10:15 +0300

    improved code

--- finance_two_backend-main/routes/potsRoutes.js
+++ finance_two_backend-main/routes/potsRoutes.js
@@ -14,5 +14,43 @@ module.exports = (potsDB) => {
     }
   });
 
+  router.post("/", async (req, res) => {
+    try {
+      const { id, name, target, total, theme } = req.body;
+
+      if (!id || !category || amount == null) {
+        return res.status(400).json({ message: "Missing required fields" });
+      }
+
+      const existingBudget = await Budget.findOne({ id });
+
+      if (existingBudget) {
+        existingBudget.name = name;
+        existingBudget.target = target;
+        existingBudget.total = total;
+        existingBudget.theme = theme;
+
+        await existingBudget.save();
+        console.log("✅ Budget updated:", existingBudget);
+        return res.status(200).json(existingBudget);
+      } else {
+        const newBudget = new Budget({
+          id,
+          name,
+          target,
+          total,
+          theme,
+        });
+
+        await newBudget.save();
+        console.log("✅ New budget added:", newBudget);
+        return res.status(201).json(newBudget);
+      }
+    } catch (error) {
+      console.error("❌ Error adding/updating budget:", error);
+      res.status(500).json({ message: "Failed to add/update budget", error });
+    }
+  });
+
   return router;
 };
